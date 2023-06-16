1288c0bd5035e430082e61674b8c07fe651a2e29
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-16 20:10:09 -0300

    finaliza requisito 3

--- semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise3/Palindrome.java
+++ semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise3/Palindrome.java
@@ -1,5 +1,11 @@
 package com.semana1.exercises.exercise3;
 
 public class Palindrome {
-    
+    public boolean isPalindrome(String word) {
+        String reverseWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverseWord += word.charAt(i);
+        }
+        return word.equals(reverseWord);
+    }
 }
