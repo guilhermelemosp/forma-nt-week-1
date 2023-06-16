153422b712ba4b384dce8f9b8712c09cf333c0ff
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-16 18:47:32 -0300

    requisito 1 concluído

--- semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise1/PairNumbers.java
+++ semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise1/PairNumbers.java
@@ -1,5 +1,16 @@
 package com.semana1.exercises.exercise1;
 
 public class PairNumbers {
-    
+    public String setPairNumbers() {
+        String pairNumbers = "";
+
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                pairNumbers += i + " ";
+            }
+        }
+
+        return pairNumbers;
+    }
+
 }
