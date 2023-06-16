82dd13f3611980606888edb073694d15ecc82fd6
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-16 19:29:50 -0300

    finaliza Requisito 2

--- semana_1_forma_nt/src/test/java/com/example/exercisesTests/exerciseTest2/PrimeNumbersTest.java
+++ semana_1_forma_nt/src/test/java/com/example/exercisesTests/exerciseTest2/PrimeNumbersTest.java
@@ -1,5 +1,12 @@
 package com.example.exercisesTests.exerciseTest2;
 
+import com.semana1.exercises.exercise2.PrimeNumbers;
+
 public class PrimeNumbersTest {
-    
+    public static void main(String[] args) {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        String primeNumbersString = primeNumbers.primeNumbersString();
+        System.out.println(primeNumbersString);
+    }
+
 }
