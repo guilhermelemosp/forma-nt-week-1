08098595dca8fd6a1f0d12191bd32198abc89295
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-16 21:10:58 -0300

    finaliza requisito 04

--- semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise4/DriverInfo.java
+++ semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise4/DriverInfo.java
@@ -1,5 +1,46 @@
 package com.semana1.exercises.exercise4;
 
+// Uma pessoa possui nome, cpf rg e identidade
+// Um funcionário possui nome, cpf, rg, identidade, idade, cargo e salário
+// Um motorista possui nome, cpf, rg, identidade, idade, cargo, salário, cnh e placa do carro
+// Monte a estrutura de classes, instancie dois motoristas e imprima no console o nome, cargo e placa do carro de cada um deles
+
 public class DriverInfo {
+    private String name;
+    private String cpf;
+    private String id;
+    private String age;
+    private String job;
+    private String salary;
+    private String driversLicense;
+    private String licenseplate;
+
+    public void getPersonInfo(String name, String cpf, String id, String age) {
+        this.name = name;
+        this.cpf = cpf;
+        this.id = id;
+        this.age = age;
+    }
+
+    public void getEmployeeInfo(String name, String cpf, String id, String age, String job, String salary) {
+        this.getPersonInfo(name, cpf, id, age);
+        this.job = job;
+        this.salary = salary;
+    }
+
+    public void getDriver(String name, String cpf, String id, String age, String job, String salary, String driversLicense, String licenseplate) {
+        this.getEmployeeInfo(name, cpf, id, age, job, salary);
+        this.driversLicense = driversLicense;
+        this.licenseplate = licenseplate;
+    }
+
+    public void printDriver(DriverInfo driveriInfo) {
+        System.out.println("Nome: " + driveriInfo.name);
+        System.out.println("Cargo: " + driveriInfo.job);
+        System.out.println("Placa do carro: " + driveriInfo.licenseplate);
+    }
+
+   
     
 }
+
--- semana_1_forma_nt/src/test/java/com/example/exercisesTests/exerciseTest4/DriverInfoTest.java
+++ semana_1_forma_nt/src/test/java/com/example/exercisesTests/exerciseTest4/DriverInfoTest.java
@@ -1,5 +1,20 @@
 package com.example.exercisesTests.exerciseTest4;
 
+import com.semana1.exercises.exercise4.DriverInfo;
+
+// 
+
+//imprima no console o nome, cargo e placa do carro de dois motoristas
 public class DriverInfoTest {
-    
+    public static void main(String[] args) {
+        DriverInfo driver1 = new DriverInfo();
+        DriverInfo driver2 = new DriverInfo();
+
+        driver1.getDriver("João", "123456789-00", "123456789", "30", "Motorista", "R$ 2.000,00", "123456789", "ABC-1234");
+        driver2.getDriver("Maria", "987654321-00", "987654321", "25", "Motorista", "R$ 2.000,00", "987654321", "XYZ-9876");
+
+        driver1.printDriver(driver1);
+        driver2.printDriver(driver2);
+      
+    }
 }
