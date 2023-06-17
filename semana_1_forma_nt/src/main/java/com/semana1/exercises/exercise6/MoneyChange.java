5a9b6fd3208855716bc55ff910c0f9f89cd4032c
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-17 15:11:42 -0300

    finaliza requisito 06

--- semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise6/MoneyChange.java
+++ semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise6/MoneyChange.java
@@ -1,5 +1,44 @@
 package com.semana1.exercises.exercise6;
 
 public class MoneyChange {
-    
+    public void printChange(double cost, double payment) {
+        double change = payment - cost;
+        System.out.println("Custo: " + cost);
+        System.out.println("--------------------");
+        System.out.println("Troco: " + change);
+        System.out.println("--------------------");
+        System.out.println("Menor Troco: ");
+        
+        int[] notes = { 100, 50, 20, 10, 5, 2, 1 };
+        double[] coins = { 1.00, 0.50, 0.25, 0.10, 0.05 };
+        boolean firstItem = true;
+
+        for (int i = 0; i < notes.length; i++) {
+            int note = notes[i];
+            while (change >= note) {
+                //se nao for o primeiro item
+                if (!firstItem) {
+                    System.out.print(", ");
+                }
+                System.out.print(note);
+                change -= note;
+                // altera o FirstItem para false
+                firstItem = false;
+            }
+        }
+        
+        for (int i = 0; i < coins.length; i++) {
+            double coin = coins[i];
+            while (change >= coin) {
+                if (!firstItem) {
+                    System.out.print(", ");
+                }
+                System.out.print(coin);
+                change -= coin;
+                
+                firstItem = false;
+            }
+        }
+        System.out.println(".");
+}
 }
