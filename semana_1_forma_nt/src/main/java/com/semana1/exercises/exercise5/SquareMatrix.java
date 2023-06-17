6622e2cb5481e46e4edae6e646331aec12abb3e6
Guilherme Lemos <guilhermelemos@MacBook-Pro-de-Guilherme.local>
2023-06-17 13:34:27 -0300

    finaliza requisito 05

--- semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise5/SquareMatrix.java
+++ semana_1_forma_nt/src/main/java/com/semana1/exercises/exercise5/SquareMatrix.java
@@ -1,5 +1,27 @@
 package com.semana1.exercises.exercise5;
 
 public class SquareMatrix {
-    
+    private int[][] matrix;
+
+    public int[][] getIdentityMatrix(int n) {
+        // Cria a estrutura
+        matrix = new int[n][n];
+        for(int i = 0; i < n; i++) {
+            matrix[i][i] = 1;
+        }
+        return matrix;
+    }
+
+    public void printMatrix(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            System.out.print("["); //abre o colchete
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j]); // printa o elemento
+                if(j < matrix[i].length - 1) { // verifica se estamos no ultimo elemento
+                    System.out.print(", "); // se não estiver no ultimo elemento, printa a virgula 
+                }
+            }
+            System.out.println("]"); // se estiver no último elemento, fecha o colchete e pula a linha
+        }
+    }
 }
--- semana_1_forma_nt/src/test/java/com/example/exercisesTests/exerciseTest5/SquareMatrixTest.java
+++ semana_1_forma_nt/src/test/java/com/example/exercisesTests/exerciseTest5/SquareMatrixTest.java
@@ -1,5 +1,11 @@
 package com.example.exercisesTests.exerciseTest5;
 
+import com.semana1.exercises.exercise5.SquareMatrix;
+
 public class SquareMatrixTest {
-    
+    public static void main(String[] args) {
+        SquareMatrix squareMatrix = new SquareMatrix();
+        int[][] matrix = squareMatrix.getIdentityMatrix(10);
+        squareMatrix.printMatrix(matrix);
+    }
 }
